b23319d81852c33c2e04b4bd7fe3e4bc0d923864
keddyboys <keddyboys@gmail.com>
2017-04-22 18:53:01 +0300

    New commit

---
+++ locale/English.php
@@ -0,0 +1,62 @@
+<?php
+//cs_panel.php
+$locale['csp_title'] = "Counter-strike panel";
+$locale['csp_desc'] = "Infusion Description";
+$locale['csp_admin1'] = "Counter-strike panel";
+$locale['csp_link1'] = "Add server";
+$locale['csp_100'] = "Counter-strike panel";
+$locale['csp_101'] = "No";
+$locale['csp_102'] = "Stats";
+$locale['csp_103'] = "Adress";
+$locale['csp_104'] = "Port";
+$locale['csp_105'] = "Players";
+$locale['csp_106'] = "Anticheat";
+$locale['csp_107'] = "Mod";
+
+//add_server.php
+$locale['csp_111'] = "Add another server";
+$locale['csp_112'] = "Return to site";
+$locale['csp_113'] = "Delete server(s)";
+$locale['csp_114'] = "Server list"; 
+$locale['csp_115'] = "We have";
+$locale['csp_116'] = "server(s) in our database.";
+$locale['csp_117'] = "Stats";
+$locale['csp_118'] = "Add server";
+$locale['csp_119a'] = "Success! Your server has been added !";
+$locale['csp_119b'] = "Error: This Server does already exist !!";
+//stats.php
+$locale['csp_120'] = "Hostname";
+$locale['csp_121'] = "Server ip";
+$locale['csp_122'] = "Game mod";
+$locale['csp_123'] = "Curent map";
+$locale['csp_124'] = "Online players";
+$locale['csp_125'] = "Secure";
+$locale['csp_126'] = "Server Os";
+$locale['csp_127'] = "Server type";
+$locale['csp_128'] = "Password?";
+$locale['csp_129'] = "Version";
+$locale['csp_130'] = "Server stats";
+$locale['csp_131'] = "Frags";
+$locale['csp_132'] = "Time";
+$locale['csp_134'] = "Yes";
+$locale['csp_135'] = "No";
+$locale['csp_136'] = "Windows";
+$locale['csp_137'] = "Linux";
+$locale['csp_138'] = "Dedicate";
+$locale['csp_139'] = "Private";
+$locale['csp_140'] = "Server Down....";
+//cs_panel_admin.php
+$locale['csp_150'] = "Option";
+$locale['csp_151'] = "Edit";
+$locale['csp_152'] = "Delete";
+$locale['csp_153'] = "Delete selected servers";
+$locale['csp_154'] = "Are you sure?";
+$locale['csp_155'] = "Save";
+
+
+
+
+
+$cod = array(1 => "Not Secure", 2 => "VAC Secure", 3 => "VAC Secure2", 4 => "HLGuard", 5 => "Cheating-Death", 6 => "Other" );
+$modul = array(1 => "Normal", 2 => "Respawn", 3 => "WAR3FT", 4 => "Heroes", 5 => "Other" );
+?>
---
+++ locale/Romanian.php
@@ -0,0 +1,52 @@
+<?php
+//cs_panel.php
+$locale['csp_title'] = "Counter-strike panel";
+$locale['csp_desc'] = "Descriere infuzie";
+$locale['csp_admin1'] = "Counter-strike panel";
+$locale['csp_link1'] = "Adaug&#259; server";
+$locale['csp_100'] = "Counter-strike panel";
+$locale['csp_101'] = "Nr.";
+$locale['csp_102'] = "Status";
+$locale['csp_103'] = "Adresa";
+$locale['csp_104'] = "Port";
+$locale['csp_105'] = "Juc&#259;tori";
+$locale['csp_106'] = "Anticheat";
+$locale['csp_107'] = "Isp";
+$locale['csp_108'] = "Mod";
+$locale['csp_109'] = "&#354;ara";
+//add_server.php
+$locale['csp_111'] = "Adaug&#259; alt server";
+$locale['csp_112'] = "Pagina principala";
+$locale['csp_113'] = "&#350;terge&#355;i server(e).";
+$locale['csp_114'] = "Lista server";
+$locale['csp_115'] = "Avem ";
+$locale['csp_116'] = "server(e) in baza noastr&#259; de date.";
+$locale['csp_117'] = "Status";
+$locale['csp_118'] = "Adaug&#259; server";
+//stats.php
+$locale['csp_120'] = "Nume";
+$locale['csp_121'] = "Server ip";
+$locale['csp_122'] = "Game mod";
+$locale['csp_123'] = "Harta curent&#259;";
+$locale['csp_124'] = "Juc&#259;tori online";
+$locale['csp_125'] = "Securizat";
+$locale['csp_126'] = "Sistem operare server";
+$locale['csp_127'] = "Tip server";
+$locale['csp_128'] = "Parol&#259;?";
+$locale['csp_129'] = "Versiune";
+$locale['csp_130'] = "Status server";
+$locale['csp_131'] = "Fraguri";
+$locale['csp_132'] = "Timp";
+$locale['csp_133'] = "&#350;terge";
+$locale['csp_134'] = "Da";
+$locale['csp_135'] = "Nu";
+$locale['csp_136'] = "Windows";
+$locale['csp_137'] = "Linux";
+$locale['csp_138'] = "Dedicat";
+$locale['csp_139'] = "Privat";
+$locale['csp_140'] = "Server picat";
+$cod = array(1 => "Not Secure", 2 => "VAC Secure", 3 => "VAC Secure2", 4 => "HLGuard", 5 => "Cheating-Death", 6 => "Other" );
+$net = array(1 => "Other", 2 => "Decknet", 3 => "Euroweb", 4 => "Fibernet", 5 => "Allnet", 6 => "Idilis", 7 => "iNES Group", 8 => "Infogate", 9 => "Mediasat", 10 => "RDS", 11 => "Romtelecom" );
+$modul = array(1 => "Normal", 2 => "Respawn", 3 => "WAR3FT", 4 => "Heroes", 5 => "Other" );
+$tara = array(1 => "RO", 2 => "FR", 3 => "DE", 4 => "IT", 5 => "ES", 6 => "GB", 7 => "US", 8=>"UE" );
+?>
